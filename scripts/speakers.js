e0097fa3da8e5d777bc266afcbd40295a5cd6eea
TekyRey <mwakabayah@gmail.com>
2022-06-24 02:07:11 +0300

    fix linter errors

--- scripts/speakers.js
+++ scripts/speakers.js
@@ -2,47 +2,47 @@ const speakerSection = document.querySelector('.subsection');
 
 const speakersProgram = [
   {
-    image: '../assets/images/speaker4.jpg',
-    icon: '../assets/images/pattern.jpg',
+    image: './assets/images/speaker4.jpg',
+    icon: './assets/images/pattern.jpg',
     name: 'Rehema Mwaka',
     label: 'Founder and CEO of WIT',
     description:
       'Rehema is the founder and CEO of the women in tech conference. She is also a Passionate software developer who wants to make the world a better place',
   },
   {
-    image: '../assets/images/speaker3.jpg',
-    icon: '../assets/images/pattern.jpg',
+    image: './assets/images/speaker3.jpg',
+    icon: './assets/images/pattern.jpg',
     name: 'Calveline Waching',
     label: 'CEO of Amazing Cal',
     description:
       'Calveline is the founder and CEO of the Amazing Cal company. She is also a Passionate software developer who wants to make the world a better place',
   },
   {
-    image: '../assets/images/speaker4.jpg',
-    icon: '../assets/images/pattern.jpg',
+    image: './assets/images/speaker4.jpg',
+    icon: './assets/images/pattern.jpg',
     name: 'Hellen Wainaina',
     label: 'Product Manager',
     description:
       'Hellen is a passionate product manager, currently making great things happen at Google',
   },
   {
-    image: '../assets/images/speaker3.jpg',
-    icon: '../assets/images/pattern.jpg',
+    image: './assets/images/speaker3.jpg',
+    icon: './assets/images/pattern.jpg',
     name: 'Rose Tinashe',
     label: 'Quality Assuarace',
     description: 'Rose is a professional Quality Assurance engineer at Helleka',
   },
   {
-    image: '../assets/images/speaker4.jpg',
-    icon: '../assets/images/pattern.jpg',
+    image: './assets/images/speaker4.jpg',
+    icon: './assets/images/pattern.jpg',
     name: 'Martha Obwaka',
     label: 'Software Marketer',
     description:
       'Martha is a top tier software marketer who has managed to market and successfully sell over 100 products',
   },
   {
-    image: '../assets/images/speaker3.jpg',
-    icon: '../assets/images/pattern.jpg',
+    image: './assets/images/speaker3.jpg',
+    icon: './assets/images/pattern.jpg',
     name: 'Rehema Mwaka',
     label: 'Founder and CEO of Women in Tech',
     description:
